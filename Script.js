3024eb437e6109fa6ea44ae4ada5474b94e08b41
John Pascua <117547584+JmPascua@users.noreply.github.com>
2025-08-26 13:14:30 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -12,6 +12,35 @@ function jump()
     },300);
 }
 
+//The eventlistener command adds the action
+document.addEventListener('keydown',function (e)
+{
+    //this line prevents pressing and holding to spam jump
+    if (e.repeat) return;
+
+    //if you're typing else where it will not trigger the jump for the game
+    const t = e.target;
+    const typing = 
+        t.tagName === 'INPUT' ||
+        t.tagName === 'TEXTAREA' ||
+        t.isContentEditable === true;
+    
+    if (typing) return;
+
+    //this chunk of code will detect whether space is being used across other pages or browsers
+    const isSpace = 
+        e.code === 'Space' ||
+        e.key === ' ' ||
+        e.keyCode === 32;
+
+    //if statement prevents the page from scrolling when space is used outside of a typing field
+    if (isSpace)
+    {
+        e.preventDefault();
+        jump();
+    }
+});
+
 var checkDead = setInterval(function() 
 {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
